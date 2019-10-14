18c7c6e83e23630163e1db71a10d1f8be36b1efb
Nikola Gavrić <nikola.gavric94@gmail.com>
2019-10-14 10:13:58 +0200

    Change method name from transactions to payments with return data

--- src/facades/Square.php
+++ src/facades/Square.php
@@ -3,12 +3,11 @@
 namespace Nikolag\Square\Facades;
 
 use Illuminate\Support\Facades\Facade;
-use Nikolag\Square\Models\Transaction;
 
 /**
  * @method static \Nikolag\Square\SquareService save()
  * @method static \Nikolag\Square\Models\Transaction charge(array $options)
- * @method static \SquareConnect\Model\ListLocationsResponse transactions(array $options)
+ * @method static \SquareConnect\Model\ListPaymentsResponse payments(array $options)
  * @method static mixed getCustomer()
  * @method static void setCustomer($customer)
  * @method static mixed getMerchant()
