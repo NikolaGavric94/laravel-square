468326ac6e20bfe95b6b22fa44d5a141fcb4a2b3
mbingham <mark.bingham@interworks.com>
2024-03-19 12:46:56 -0500

    adds fulfillment pivot

---
+++ src/database/migrations/2024_03_19_172925_create_nikolag_fulfillment_order_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('nikolag_fulfillment_order', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('fulfillment_id');
+            $table->string('order_id', 25);
+        });
+
+        Schema::table('nikolag_fulfillment_order', function (Blueprint $table) {
+            $table->foreign('fulfillment_id', 'fulfill_id')->references('id')->on('nikolag_fulfillments');
+            $table->unique(['fulfillment_id', 'order_id'], 'fulfillid_ordid');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('nikolag_product_order');
+    }
+}
