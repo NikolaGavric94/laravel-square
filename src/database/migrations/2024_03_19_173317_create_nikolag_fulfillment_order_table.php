8c8900fa0d1c6c138b0b528832aed080c4f4da2e
mbingham <mark.bingham@interworks.com>
2024-03-29 17:36:30 -0500

    adds type in case I need it

--- src/database/migrations/2024_03_19_173317_create_nikolag_fulfillment_order_table.php
+++ src/database/migrations/2024_03_19_173317_create_nikolag_fulfillment_order_table.php
@@ -16,12 +16,13 @@ return new class extends Migration
         Schema::create('nikolag_fulfillment_order', function (Blueprint $table) {
             $table->increments('id');
             $table->unsignedInteger('fulfillment_id');
+            $table->string('fulfillment_type', 25);
             $table->string('order_id', 25);
         });
 
         Schema::table('nikolag_fulfillment_order', function (Blueprint $table) {
             $table->foreign('fulfillment_id', 'fulfill_id')->references('id')->on('nikolag_fulfillments');
-            $table->unique(['fulfillment_id', 'order_id'], 'fulfillid_ordid');
+            $table->unique(['fulfillment_id', 'order_id', 'fulfillment_type'], 'fulfillid_ordid');
         });
     }
 
