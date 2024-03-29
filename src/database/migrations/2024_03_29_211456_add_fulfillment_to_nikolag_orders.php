902ad4187c828d566e46e1f98202c502b538927e
mbingham <mark.bingham@interworks.com>
2024-03-29 16:17:23 -0500

    adds fulfillment data

---
+++ src/database/migrations/2024_03_29_211456_add_fulfillment_to_nikolag_orders.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('nikolag_orders', function (Blueprint $table) {
+            $table->unsignedBigInteger('fulfillment_id')->nullable();
+            $table->string('fulfillment_type', 25)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('nikolag_orders', function (Blueprint $table) {
+            $table->dropColumn('fulfillment_id');
+            $table->dropColumn('fulfillment_type');
+        });
+    }
+};
