4c234f4928223e2ab453fbea77df47259dae052b
mbingham <mark.bingham@interworks.com>
2024-03-29 22:05:57 -0500

    typo fix

--- src/traits/HasFulfillments.php
+++ src/traits/HasFulfillments.php
@@ -24,7 +24,7 @@ trait HasFulfillments
 
     /**
      * Return a list of fulfillments which are associated with order.
-     *
+     *e
      * @return BelongsToMany
      */
     public function fulfillments(): BelongsToMany
