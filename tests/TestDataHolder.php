9f148e7cf620505b881d26fae062ddcc7cc4b160
mbingham <mark.bingham@interworks.com>
2024-05-15 22:45:32 -0500

    adds pickup detail test

--- tests/TestDataHolder.php
+++ tests/TestDataHolder.php
@@ -4,40 +4,51 @@ namespace Nikolag\Square\Tests;
 
 use Nikolag\Square\Models\Customer;
 use Nikolag\Square\Models\Discount;
+use Nikolag\Square\Models\Fulfillment;
+use Nikolag\Square\Models\PickupDetails;
 use Nikolag\Square\Models\Product;
 use Nikolag\Square\Models\Tax;
+use Nikolag\Square\Utils\Constants;
 use Nikolag\Square\Tests\Models\Order;
 use Nikolag\Square\Tests\Models\User;
 
 class TestDataHolder
 {
-    public function __construct(public ?Order $order,
-                                public ?Product $product,
-                                public ?Customer $customer,
-                                public ?User $merchant,
-                                public ?Tax $tax,
-                                public ?Discount $discount)
-    {
+    public function __construct(
+        public ?Order $order,
+        public ?Product $product,
+        public ?Customer $customer,
+        public ?User $merchant,
+        public ?Tax $tax,
+        public ?Discount $discount,
+        public ?Fulfillment $pickupDetails,
+    ) {
     }
 
     public static function make(): self
     {
-        return new static(factory(Order::class)->make(),
+        return new static(
+            factory(Order::class)->make(),
             factory(Product::class)->make(),
             factory(Customer::class)->make(),
             factory(User::class)->make(),
             factory(Tax::class)->make(),
-            factory(Discount::class)->states('AMOUNT_ONLY')->make());
+            factory(Discount::class)->states('AMOUNT_ONLY')->make(),
+            factory(Fulfillment::class)->states(Constants::FULFILLMENT_TYPE_PICKUP)->make(),
+        );
     }
 
     public static function create(): self
     {
-        return new static(factory(Order::class)->create(),
+        return new static(
+            factory(Order::class)->create(),
             factory(Product::class)->create(),
             factory(Customer::class)->create(),
             factory(User::class)->create(),
             factory(Tax::class)->create(),
-            factory(Discount::class)->states('AMOUNT_ONLY')->create());
+            factory(Discount::class)->states('AMOUNT_ONLY')->create(),
+            factory(Fulfillment::class)->states(Constants::FULFILLMENT_TYPE_PICKUP)->make(),
+        );
     }
 
     public function modify(string $prodFac = 'create',
