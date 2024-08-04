43ef9f5607581bb3f80e0e9d719b19b5daa32f92
mbingham <mark.bingham@interworks.com>
2024-08-04 10:02:30 -0500

    udpates exception message

--- src/SquareService.php
+++ src/SquareService.php
@@ -199,7 +199,7 @@ class SquareService extends CorePaymentService implements SquareServiceContract
         } catch (MissingPropertyException $e) {
             throw new MissingPropertyException('Required fields are missing', 500, $e);
         } catch (InvalidSquareOrderException $e) {
-            throw new MissingPropertyException('Required column is missing from the table', 500, $e);
+            throw new MissingPropertyException('Invalid order data', 500, $e);
         } catch (Exception|ApiException $e) {
             throw new Exception('There was an error with the api request', 500, $e);
         }
@@ -281,7 +281,7 @@ class SquareService extends CorePaymentService implements SquareServiceContract
             } catch (MissingPropertyException $e) {
                 throw new MissingPropertyException('Required field is missing', 500, $e);
             } catch (InvalidSquareOrderException $e) {
-                throw new MissingPropertyException('Required column is missing from the table', 500, $e);
+                throw new MissingPropertyException('Invalid order data', 500, $e);
             } catch (Exception $e) {
                 throw new Exception('There was an error with the api request', 500, $e);
             }
