39be06e20f508b89ed675b6ba75613fa17d7a73f
mbingham <mark.bingham@interworks.com>
2024-08-04 10:02:20 -0500

    adds id conflict

--- src/builders/OrderBuilder.php
+++ src/builders/OrderBuilder.php
@@ -51,7 +51,14 @@ class OrderBuilder
         // Set location if it's not included in the order
         if (!$order->location_id) {
             $order->location_id = $orderCopy->location_id;
+        } elseif ($order->location_id != $orderCopy->location_id) {
+            throw new InvalidSquareOrderException(
+                'Location ID conflict.  Order Data: ' . $order->location_id
+                    . '. Order Copy: ' . $orderCopy->location_id,
+                500
+            );
         }
+
         // Save order first
         $order->save();
 
