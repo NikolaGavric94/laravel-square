577d0bdb204d15af094184256c75d775858b12d0
mbingham <mark.bingham@interworks.com>
2025-02-01 10:13:33 -0600

    uses model constants

--- src/database/migrations/2024_03_19_172925_create_nikolag_fulfillments_table.php
+++ src/database/migrations/2024_03_19_172925_create_nikolag_fulfillments_table.php
@@ -3,6 +3,8 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Square\Models\FulfillmentState;
+use Square\Models\FulfillmentType;
 
 return new class extends Migration
 {
@@ -16,10 +18,17 @@ return new class extends Migration
         Schema::create('nikolag_fulfillments', function (Blueprint $table) {
             $table->id();
             $table->string('order_id');
-            $table->enum('type', ['PICKUP', 'SHIPMENT', 'DELIVERY']);
-            $table->enum('state', ['PROPOSED', 'RESERVED', 'PREPARED', 'COMPLETED', 'CANCELED', 'FAILED'])->nullable();
+            $table->enum('type', [FulfillmentType::PICKUP, FulfillmentType::SHIPMENT, FulfillmentType::DELIVERY]);
+            $table->enum('state', [
+                FulfillmentState::PROPOSED,
+                FulfillmentState::RESERVED,
+                FulfillmentState::PREPARED,
+                FulfillmentState::COMPLETED,
+                FulfillmentState::CANCELED,
+                FulfillmentState::FAILED
+            ])->nullable();
             $table->string('uid', 60)->nullable();
-            // Adds fulfillment_details_id and fulfillment_details_type columns and index
+            // Adds fulfillment_details_id, fulfillment_details_type columns and index
             $table->morphs('fulfillment_details');
             $table->timestamps();
         });
