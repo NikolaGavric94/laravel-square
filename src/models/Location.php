bf679af947067b7a082cbe1067d541c772f5d3c1
mbingham <mark.bingham@interworks.com>
2025-02-05 14:31:31 -0600

    moves location sync data parsing to model

--- src/models/Location.php
+++ src/models/Location.php
@@ -2,7 +2,9 @@
 
 namespace Nikolag\Square\Models;
 
+use DateTime;
 use Illuminate\Database\Eloquent\Model;
+use Square\Models\Location as SquareLocation;
 
 class Location extends Model
 {
@@ -25,4 +27,36 @@ class Location extends Model
         'created_at' => 'datetime',
         'updated_at' => 'datetime',
     ];
+
+    /**
+     * Processes the location data during sync
+     *
+     * @param array $locationData The json serialized location data from the REST API.
+     *
+     * @var array
+     */
+    public static function processLocationData(SquareLocation $location): array
+    {
+        $locationData = $location->jsonSerialize();
+
+        // Remove the ID and set it as the square_id
+        $locationData['square_id'] = $locationData['id'];
+        unset($locationData['id']);
+
+        // Update columns that are stores as more complex objects
+        $locationData['address'] = json_encode($location->getAddress()?->jsonSerialize());
+        $locationData['capabilities'] = json_encode($location->getCapabilities());
+        $locationData['business_hours'] = json_encode($location->getBusinessHours()?->jsonSerialize());
+
+        // Cast any dates from the model
+        $emptyModel = new self();
+        foreach ($emptyModel->casts as $key => $value) {
+            if ($value == 'datetime' && isset($locationData[$key])) {
+                $value = new DateTime($locationData[$key]);
+                $locationData[$key] = $value->format('Y-m-d H:i:s');
+            }
+        }
+
+        return $locationData;
+    }
 }
