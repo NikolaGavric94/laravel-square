077347758d676f62e3fbb98ed9a5286d148c4e6e
mbingham <mark.bingham@interworks.com>
2025-02-25 17:37:23 -0600

    adds modifier and option test

--- tests/unit/ModifierTest.php
+++ tests/unit/ModifierTest.php
@@ -61,4 +61,33 @@ class ModifierTest extends TestCase
         $this->assertNotEmpty($orderProduct->modifier);
         $this->assertInstanceOf(Modifier::class, $orderProduct->modifier);
     }
+
+    /**
+     * Check Modifier persisting with orders.
+     *
+     * @return void
+     */
+    public function test_modifier_and_option_create_with_product_order(): void
+    {
+        $name = $this->faker->name;
+
+        // Create a new modifier and modifier option
+        $modifier = factory(Modifier::class)->create([
+            'name' => $name,
+        ]);
+        $modifierOption = factory(ModifierOption::class)->create([
+            'name' => $name,
+            'modifier_id' => $modifier->id,
+        ]);
+
+        // This factory also creates a new order and product
+        $orderProduct = factory(OrderProductPivot::class)->create();
+        $orderProduct->modifier()->associate($modifier->id);
+        $orderProduct->modifierOption()->associate($modifierOption->id);
+
+        $this->assertNotEmpty($orderProduct->modifier);
+        $this->assertInstanceOf(Modifier::class, $orderProduct->modifier);
+        $this->assertNotEmpty($orderProduct->modifierOption);
+        $this->assertInstanceOf(ModifierOption::class, $orderProduct->modifierOption);
+    }
 }
