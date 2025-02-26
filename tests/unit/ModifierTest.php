8ff3cbd7e78738b37c7187e4fb3f95580ccb22f6
mbingham <mark.bingham@interworks.com>
2025-02-25 18:48:56 -0600

    adds modifier tests

--- tests/unit/ModifierTest.php
+++ tests/unit/ModifierTest.php
@@ -2,9 +2,14 @@
 
 namespace Nikolag\Square\Tests\Unit;
 
+use Exception;
+use Nikolag\Square\Facades\Square;
 use Nikolag\Square\Models\Modifier;
 use Nikolag\Square\Models\ModifierOption;
+use Nikolag\Square\Models\ModifierOptionLocationPivot;
+use Nikolag\Square\Models\OrderProductModifierPivot;
 use Nikolag\Square\Models\OrderProductPivot;
+use Nikolag\Square\Models\Product;
 use Nikolag\Square\Tests\Models\Order;
 use Nikolag\Square\Tests\TestCase;
 
@@ -38,56 +43,5 @@ class ModifierTest extends TestCase
         $this->assertDatabaseHas('nikolag_modifiers', [
             'name' => $name,
         ]);
-    }
-
-    /**
-     * Check Modifier persisting with orders.
-     *
-     * @return void
-     */
-    public function test_modifier_create_with_product_order(): void
-    {
-        $name = $this->faker->name;
-
-        // Create a new modifier and modifier option
-        $modifier = factory(Modifier::class)->create([
-            'name' => $name,
-        ]);
-
-        // This factory also creates a new order and product
-        $orderProduct = factory(OrderProductPivot::class)->create();
-        $orderProduct->modifier()->associate($modifier->id);
-
-        $this->assertNotEmpty($orderProduct->modifier);
-        $this->assertInstanceOf(Modifier::class, $orderProduct->modifier);
-    }
-
-    /**
-     * Check Modifier persisting with orders.
-     *
-     * @return void
-     */
-    public function test_modifier_and_option_create_with_product_order(): void
-    {
-        $name = $this->faker->name;
-
-        // Create a new modifier and modifier option
-        $modifier = factory(Modifier::class)->create([
-            'name' => $name,
-        ]);
-        $modifierOption = factory(ModifierOption::class)->create([
-            'name' => $name,
-            'modifier_id' => $modifier->id,
-        ]);
-
-        // This factory also creates a new order and product
-        $orderProduct = factory(OrderProductPivot::class)->create();
-        $orderProduct->modifier()->associate($modifier->id);
-        $orderProduct->modifierOption()->associate($modifierOption->id);
-
-        $this->assertNotEmpty($orderProduct->modifier);
-        $this->assertInstanceOf(Modifier::class, $orderProduct->modifier);
-        $this->assertNotEmpty($orderProduct->modifierOption);
-        $this->assertInstanceOf(ModifierOption::class, $orderProduct->modifierOption);
     }
 }
