a6884babd51c3b3dc90f741613fa29c71c1e0bd2
mrkbingham <mrkbingham@gmail.com>
2025-06-24 11:04:05 -0500

    simple verification

--- tests/unit/WebhookVerifierTest.php
+++ tests/unit/WebhookVerifierTest.php
@@ -6,6 +6,7 @@ use Nikolag\Square\Exceptions\InvalidSquareSignatureException;
 use Nikolag\Square\Tests\TestCase;
 use Nikolag\Square\Utils\Constants;
 use Nikolag\Square\Utils\WebhookVerifier;
+use Nikolag\Square\Models\WebhookSubscription;
 
 class WebhookVerifierTest extends TestCase
 {
@@ -51,5 +52,69 @@ class WebhookVerifierTest extends TestCase
                 ]
             ]
         ];
+    }
+
+    public function test_verify_returns_true_for_valid_signature()
+    {
+        $signature = hash_hmac('sha256', $this->testNotificationUrl . $this->validPayload, $this->testSignatureKey);
+
+        $result = WebhookVerifier::verify(
+            $this->validPayload,
+            $signature,
+            $this->testSignatureKey,
+            $this->testNotificationUrl
+        );
+
+        $this->assertTrue($result);
+    }
+
+    public function test_verify_returns_false_for_invalid_signature()
+    {
+        $invalidSignature = 'invalid-signature-hash';
+
+        $result = WebhookVerifier::verify(
+            $this->validPayload,
+            $invalidSignature,
+            $this->testSignatureKey,
+            $this->testNotificationUrl
+        );
+
+        $this->assertFalse($result);
+    }
+
+    public function test_verify_returns_false_for_tampered_payload()
+    {
+        $signature = hash_hmac('sha256', $this->testNotificationUrl . $this->validPayload, $this->testSignatureKey);
+        $tamperedPayload = str_replace('order-456', 'order-999', $this->validPayload);
+
+        $result = WebhookVerifier::verify(
+            $tamperedPayload,
+            $signature,
+            $this->testSignatureKey,
+            $this->testNotificationUrl
+        );
+
+        $this->assertFalse($result);
+    }
+
+    public function test_verify_returns_false_for_wrong_notification_url()
+    {
+        $signature = hash_hmac('sha256', $this->testNotificationUrl . $this->validPayload, $this->testSignatureKey);
+        $wrongUrl = 'https://malicious.com/webhook';
+
+        $result = WebhookVerifier::verify(
+            $this->validPayload,
+            $signature,
+            $this->testSignatureKey,
+            $wrongUrl
+        );
+
+        $this->assertFalse($result);
+    }
+
+    public function test_verify_handles_empty_strings()
+    {
+        $result = WebhookVerifier::verify('', '', '', '');
+        $this->assertFalse($result);
     }
 }
