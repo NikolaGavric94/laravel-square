76d63afea23929fcc16d9ebaf159a8eb792c4342
mrkbingham <mrkbingham@gmail.com>
2025-07-01 14:07:37 -0500

    makes uid unique

--- src/database/migrations/2024_03_19_172925_create_nikolag_fulfillments_table.php
+++ src/database/migrations/2024_03_19_172925_create_nikolag_fulfillments_table.php
@@ -20,7 +20,7 @@ return new class extends Migration
             $table->unsignedBigInteger('order_id');
 
             // Square-specific fields
-            $table->string('uid', 60)->nullable();
+            $table->string('uid', 60)->unique()->nullable();
             $table->enum('type', [FulfillmentType::PICKUP, FulfillmentType::SHIPMENT, FulfillmentType::DELIVERY]);
             $table->enum('state', [
                 FulfillmentState::PROPOSED,
