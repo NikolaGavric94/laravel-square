1083a5b17421dca5e2033f9e6debe8b82b25b8af
mrkbingham <mrkbingham@gmail.com>
2025-07-08 15:12:25 -0500

    updates migrations

--- src/database/migrations/2024_03_19_213214_create_nikolag_delivery_details_table.php
+++ src/database/migrations/2024_03_19_213214_create_nikolag_delivery_details_table.php
@@ -14,8 +14,7 @@ return new class extends Migration
     {
         Schema::create('nikolag_delivery_details', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('nikolag_fulfillment_id')->nullable()
-                ->constrained('nikolag_fulfillments')->cascadeOnDelete();
+            $table->foreignId('fulfillment_id')->nullable()->constrained('nikolag_fulfillments')->cascadeOnDelete();
 
             // Square Order Fulfillment Delivery Details
             $table->string('fulfillment_uid', 60)->nullable()->unique();
